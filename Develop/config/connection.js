71c3a1c4be5dd59fbc3c3ddaec7a9c9dc62e39ca
Dalia Gomez <daivogg@gmail.com>
2024-08-22 21:11:19 -0600

    Performed changes in connection.js, category.js, product.js and index.js

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
-      dialect: 'postgres',
+      dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
